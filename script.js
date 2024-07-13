9670408e2cea7b720b6f0e794b0a898d52b1f0c8
ansh2250 <anshsinha947@gmail.com>
2024-07-13 12:22:22 +0530

    added file

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+
+var name = "Faizan"; //var is function-scoped and can be re-declared and updated.
+let age = 25; //let is block-scoped and can be updated but not re-declared.
+const isStudent= true; //const is block-scoped and cannot be updated or re-declared.
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
